d5cdc978d94076291f7349fffa315a3d8524ad24
Thiago da Silva Gracini <thiago.gracini@outlook.com>
2023-01-18 07:23:24 -0300

    Página homeNoAuth finalizada. Toda responsividade e também as seções funcionais. Conexão com o backend e utilização do getStaticProps

---
+++ src/components/common/footer/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss'
+import { Container } from 'reactstrap'
+
+const Footer = function () {
+  return <>
+    <Container className={styles.footer}>
+      <img
+        src="/logoOnebitcode.svg"
+        alt="logoFooter"
+        className={styles.footerLogo}
+      />
+      <a href="http://onebitcode.com" target={"blank"} className={styles.footerLink}>ONEBITCODE.COM</a>
+    </Container>
+  </>
+}
+
+export default Footer
